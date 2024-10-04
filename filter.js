fadb64425d56675d3020fdb1f09e7352ba071a3a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-04 18:02:14 +0600

    filter example

--- filter.js
+++ filter.js
@@ -5,3 +5,7 @@ const players = [75, 65, 67, 72, 55, 59];
 // const selected = players.filter(p => p > 80);
 const selected = players.filter(p => p % 2 === 1)
 console.log(selected);
+
+const friends = ['tom', 'john', 'michel', 'josna', 'sanzida'];
+const oddFriends = friends.filter(friend => friend.length > 4)
+console.log(oddFriends);
