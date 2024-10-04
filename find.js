045da67711730f5b4bad2ebb869ccb01b0578cc5
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-04 18:08:46 +0600

    find example

---
+++ find.js
@@ -0,0 +1,11 @@
+const players = [75, 65, 67, 72, 55, 59];
+
+const selected = players.find(player => player > 70)
+console.log(selected);
+
+/**
+ * 1. map
+ * 2. foreach
+ * 3. filter
+ * 4. find
+ */
